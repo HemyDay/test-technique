8b485598c3310f5fe474ee2040a163b22d189d41
HemyDay <sire.amanda@gmail.com>
2024-10-30 11:10:29 +0100

    Partie 3

--- src/Components/Slot.tsx
+++ src/Components/Slot.tsx
@@ -1,5 +1,5 @@
 // LIBRARIES
-import React from "react";
+import React, { useState } from "react"
 // STYLE
 import './Slot.css'
 // COMPONENTS
@@ -8,9 +8,32 @@ import './Slot.css'
 // COMPONENT START
 const Slot = (props:any) => {
 
+  const [clickCount, setClickCount] = useState(0);
+
+  const handleClick = () => {
+    setClickCount(prevCount => prevCount + 1);
+  };
+
+  const getBackgroundColor = () => {
+    switch (clickCount % 4) {
+      case 1:
+        return 'black';
+      case 2:
+        return 'green';
+      case 3:
+        return 'blue';
+      default:
+        return 'whitesmoke';
+    }
+  };
+
   return (
-    <p className="Slot">
-      {props.date.start.toLocaleTimeString('fr-FR', { hour:'numeric', minute:'numeric'})}
+    <p 
+      className="Slot"
+      onClick={handleClick}
+      style={{ backgroundColor: getBackgroundColor() }}
+    >
+      {props.date.start.toLocaleTimeString('fr-FR', {hour:'numeric', minute:'numeric'})}
     </p>
   )
 }
